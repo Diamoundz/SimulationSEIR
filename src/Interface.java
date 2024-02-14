8db5ce8ae6cafa73a8f99a287e613a2125bb3a24
Filorca <elijahcartelli@gmail.com>
2024-02-14 16:18:09 +0100

    -m "add interface"

---
+++ src/Interface.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import javax.swing.JFrame;
+
+public class Interface {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Test Frame");
+        frame.setVisible(true);
+    }
+}
