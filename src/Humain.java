98da9e3ebeed43ec9396f77745f328bf784a3200
jeengelbre <jeengelbre@a208pc07L.local.isima.fr>
2024-02-14 16:17:55 +0100

    Made Human Class

---
+++ src/Humain.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Humain {
+    public double dE;
+    public double dI;
+    public double dR;
+
+    public Humain(double dE, double dI, double dR){
+        this.dE = dE;
+        this.dI = dI;
+        this.dR = dR;
+    }
+}
